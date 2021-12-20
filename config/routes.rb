a6f25315a953e50b46c6c231c4ec8a434416b9d9
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2021-12-20 07:08:09 +0530

    Update Gemfile for Heroku

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "application#hello"
+
+  # DSL => _VERB_ "_route_", to: "_action_"
+end
