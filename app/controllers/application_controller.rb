ed385b2abf37c6bad5e488471152c715230d48cb
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-02-17 20:58:58 +0530

    Add new route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
     def hello
         render html: "hello 😃, world!"
     end
+
+    def tshirt
+        render html: "<h1>Tshirt</h1>"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "application#hello"
 
+  get "/tshirt", to: "application#tshirt"
+
   # DSL => _VERB_ "_route_", to: "_action_"
 end
