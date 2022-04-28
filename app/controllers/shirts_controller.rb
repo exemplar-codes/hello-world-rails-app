8f34f155a9603e380fe725c80c64e7facb67a028
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-04-28 07:27:50 +0530

    Add linter and formatter

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -1,45 +1,54 @@
 class ShirtsController < ApplicationController
-    def initialize
-        @shirts_arr = [{ id: 1, name: "Polka shirt", size: 'L' }] # per session persistence, instead of DB.
-        # So I can focus on Rails without ActiveRecord (the ORM) or DB setup
+  def initialize
+    super()
+    @shirts_arr = [{ id: 1, name: 'Polka shirt', size: 'L' }] # per session persistence, instead of DB.
+    # So I can focus on Rails without ActiveRecord (the ORM) or DB setup
+  end
+
+  skip_before_action :verify_authenticity_token
+  before_action :dbfy
+
+  def index
+    # GET - list of all items
+    render json: { shirts: @shirts_arr }
+  end
+
+  def show
+    # GET - specific (item with matching id or any other attribute), '/shirts/:id'
+    # the param string :id will be parsed and saved in 'params' object
+    desired_id = params[:id].to_i
+    desired_shirt = @shirts_arr.find { |shirt| shirt[:id] == desired_id }
+
+    if desired_shirt
+      render json: { shirt: desired_shirt }
+    else
+      render json: { message: 'Shirt not found' }
     end
+  end
 
-    skip_before_action :verify_authenticity_token
+  def new
+    # GET - Send HTML form for creating a new item (shirt), '/shirts/:id/new'
+    # ignoring for now
+    render html: 'Directly hit with a post request containing {name, size}'
+  end
 
-    def index # GET - list of all items
-        render json: { shirts: @shirts_arr }
-    end
+  def create
+    # POST - create new item, '/shirts/'
 
-    def show # GET - specific (item with matching id or any other attribute), '/shirts/:id'
-        # the param string :id will be parsed and saved in 'params' object
-        desired_id = params[:id].to_i
-        desired_shirt = @shirts_arr.find { |shirt| shirt[:id] == desired_id }
+    # here we'd need to parse the request body
+    # this is also parsed and kept in params
 
-        if desired_shirt
-            render json: { shirt: desired_shirt }
-        else
-            render json: { message: "Shirt not found" }
-        end
-    end
+    new_shirt = { id: @shirts_arr.length + 1, name: params[:name], size: params[:size] }
+    @shirts_arr.push(new_shirt)
 
-    def new # GET - Send HTML form for creating a new item (shirt), '/shirts/:id/new'
-        # ignoring for now
-        render html: "Directly hit with a post request containing {name, size}"
-    end
+    render json: { shirts: @shirts_arr }
+  end
 
-    def create # POST - create new item, '/shirts/'
-        # here we'd need to parse the request body
-        # this is also parsed and kept in params
+  def destroy; end
 
-        new_shirt = { id: @shirt_arr.length + 1, lengthname: params.name, size: params.size }
-        @shirt_arr.push(new_shirt)
+  def update; end
 
-        render json: { message: "New shirt added!" }
-    end
-
-    def destroy # DELETE - delete item with id, '/shirts/:id'
-    end
-
-    def update # PUT/PATCH - update item with id, '/shirts/:id/edit'
-    end
+  def dbfy
+    @shirts_arr.push({ db: 'dbfiy ran' })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # DSL => _VERB_ "_route_", to: "_action_"
-  root "application#hello"
+  root 'application#hello'
 
   # resources :shirts # all 7 REST actions in one go
   get '/shirts', to: 'shirts#index'
