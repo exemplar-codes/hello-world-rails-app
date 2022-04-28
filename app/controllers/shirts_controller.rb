273075d735b4d89772f0566e400b0b76f23b3a61
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-04-28 06:36:54 +0530

    Explain about the naming of REST APIs in Rails

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -4,7 +4,22 @@ class ShirtsController < ApplicationController
         # So I can focus on Rails without ActiveRecord (the ORM) or DB setup
     end
 
-    def index
+    def index # GET - list of all items
         render json: { shirts: @shirts_arr }
+    end
+
+    def show # GET - specific (item with matching id or any other attribute), '/shirts/:id'
+    end
+
+    def new # GET - Send HTML form for creating a new item (shirt), '/shirts/:id/new'
+    end
+
+    def create # POST - create new item, '/shirts/:id'
+    end
+
+    def destroy # DELETE - delete item with id, '/shirts/:id'
+    end
+
+    def update # PUT/PATCH - update item with id, '/shirts/:id/edit'
     end
 end
