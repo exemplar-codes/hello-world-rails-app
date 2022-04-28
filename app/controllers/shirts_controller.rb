9322661d5dff1749c608c0089911d81a427a3fa8
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-04-28 06:15:50 +0530

    Create very simple setup to study about Rails

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -1,5 +1,10 @@
 class ShirtsController < ApplicationController
+    def initialize
+        @shirts_arr = [{ id: 1, name: "Polka shirt", size: 'L' }] # per session persistence, instead of DB.
+        # So I can focus on Rails without ActiveRecord (the ORM) or DB setup
+    end
+
     def index
-        render json: { shirts: "All shirts" }
+        render json: { shirts: @shirts_arr }
     end
 end
