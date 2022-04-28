af77ab35bc954c061917e12c73a4e1692899ac10
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-04-28 06:06:27 +0530

    Clean up and set root URI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
     def hello
         render html: "hello 😃, world!"
     end
-
-    def tshirt
-        render html: "Tshirt"
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # DSL => _VERB_ "_route_", to: "_action_"
   root "application#hello"
 
-  get "/tshirt", to: "application#tshirt"
-
-  # DSL => _VERB_ "_route_", to: "_action_"
 end
